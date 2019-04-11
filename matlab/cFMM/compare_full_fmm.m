9e410f567fd9a3982531e312c28598daf85259bd
Michal Merta <mer126@w112-245.vsb.cz>
2019-04-11 15:09:22 +0200

    Optimization of cFMM neighbor clusters search

--- matlab/cFMM/compare_full_fmm.m
+++ matlab/cFMM/compare_full_fmm.m
@@ -1,11 +1,11 @@
 % Tests solution of a purely time-dependent problem on a sphere
 
 % levels of the binary tree
-L = 6;
+L = 10;
 % number of temporal panels per temporal cluster
-N = 10;
+N = 15;
 % end time
-T = 2;
+T = 1;
 % order of the Lagrange interpolant
 order = 3;
 % RHS function
@@ -19,11 +19,15 @@ fprintf('Solving problem with %d time-steps, end-time = %f. \n', N*2^L, T ...
   );
 
 % solve using FMM
+tic
 fmm_solver = cFMM_solver(0, T, N, L, order, rhs_fun);
 x_fmm = fmm_solver.solve( );
+toc
 
 % solve using full matrices
+tic
 x_full = causal_full( T, N*2^L, rhs_fun );
+toc
 
 ht = T / ( N * 2^L );
 t = ht / 2 : ht : T - ht / 2;
