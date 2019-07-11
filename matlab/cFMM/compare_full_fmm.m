63161250259bbad375d444915774d14c6110c809
Michal Merta <mer126@w115-34.vsb.cz>
2019-07-11 09:17:48 +0200

    Minor FMM solver modifications

--- matlab/cFMM/compare_full_fmm.m
+++ matlab/cFMM/compare_full_fmm.m
@@ -1,59 +1,50 @@
 % Tests solution of a purely time-dependent problem on a sphere
 
 % levels of the binary tree
-L = 10;
+L = 4;
 % number of temporal panels per temporal cluster
-N = 15;
+N = 20;
 % end time
 T = 1;
 % order of the Lagrange interpolant
-order = 3;
+order = 5;
 % RHS function
 rhs_fun = @( t ) ( ( exp( t ) / 4 ) ...
   .* ( exp( 2 ) * erfc( ( 1 + t ) ./ ( sqrt( t ) ) ) ...
-  + 2 * erf( sqrt( t ) - exp( -2 ) * erfc( ( 1 - t ) ... 
+  + 2 * erf( sqrt( t ) - exp( -2 ) * erfc( ( 1 - t ) ...
   ./ ( sqrt( t ) ) ) ) ) );
-%rhs_fun =@( t ) sin( 8*pi*t )*exp( -t );
+%rhs_fun =@( t ) sin( 4*pi*t )*exp( -t );
 
-fprintf('Solving problem with %d time-steps, end-time = %f. \n', N*2^L, T ...
+fprintf('\nSolving problem with %d time-steps, end-time = %f. \n', N*2^L, T ...
   );
 
 % solve using FMM
-tic
+fprintf('\nSolving using time-stepping FMM. \n');
 fmm_solver = cFMM_solver(0, T, N, L, order, rhs_fun);
+tic
 x_fmm_dir = fmm_solver.solve_direct( );
 toc
 
-tic
+fprintf('\nSolving using GMRES with temporal FMM. \n');
 fmm_solver = cFMM_solver(0, T, N, L, order, rhs_fun);
+tic
 x_fmm_iter = fmm_solver.solve_iterative( );
 toc
 
-tic
+
+fprintf('\nSolving using GMRES with standard FMM. \n');
 fmm_solver = cFMM_solver(0, T, N, L, order, rhs_fun);
+tic
 x_std_fmm_iter = fmm_solver.solve_iterative_std_fmm( );
 toc
 
 
 % solve using full matrices
+fprintf('\nSolving using dense matrices. \n');
 tic
 [ x_full, V ] = causal_full( T, N*2^L, rhs_fun );
 toc
 
-% compare multiplication
-% b = ones(size(V,2), 1);
-% tic
-% a1 = fmm_solver.apply_fmm_matrix_std(b);
-% toc
-% a2 = V * b;
-
-% VVV = zeros(size(V));
-% for i = 1 : size(V,2)
-%   aa = zeros(size(V,2), 1);
-%   aa(i) = 1;
-%   col = fmm_solver.apply_fmm_matrix(b);
-%   VVV(:, i) = col;
-% end
 
 ht = T / ( N * 2^L );
 t = ht / 2 : ht : T - ht / 2;
@@ -68,8 +59,10 @@ plot( t, x_fmm_iter );
 plot( t, x_std_fmm_iter );
 plot( t, x_full );
 
+
 %legend({'FMM','full'},'Location','southwest');
 
 % analytical solution for the first testing RHS (comment out for the other)
 plot( t, analytical );
 legend({'FMM - direct', 'FMM - iterative', 'standard FMM - iterative', 'full', 'analytical'},'Location','southwest');
+%legend({'FMM - direct', 'FMM - iterative', 'standard FMM - iterative', 'full'},'Location','southwest');
