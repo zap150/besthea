570f6a40ec878e2b548139b50cf827c3efc73b53
Jan Zapletal <jan.zapletal@vsb.cz>
2019-11-14 15:45:30 +0100

    ENH: added MKL CG and FGMRES to linear operator

--- cpp/src/uniform_spacetime_be_space.cpp
+++ cpp/src/uniform_spacetime_be_space.cpp
@@ -112,7 +112,8 @@ void besthea::bem::uniform_spacetime_be_space< basis_type >::L2_projection(
     }
     cg_eps = 1e-6;
     n_iter = 200;
-    M.eigen_cg_solve( rhs, projection.get_block( d ), cg_eps, n_iter );
+    M.mkl_fgmres_solve( rhs, projection.get_block( d ), cg_eps, n_iter, n_iter );
+    //M.eigen_cg_solve( rhs, projection.get_block( d ), cg_eps, n_iter );
     rhs.fill( 0.0 );
   }
 }
