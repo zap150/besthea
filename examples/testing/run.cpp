d1852d26f6358d8e39fd023ac8164b8c6bd333fd
Jakub Homola <jakub.homola@vsb.cz>
2021-01-20 15:25:39 +0100

    onthefly matrix basics

---
+++ examples/testing/run.cpp
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2019, VSB - Technical University of Ostrava and Graz University of
+ * Technology All rights reserved. Redistribution and use in source and binary
+ * forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer. Redistributions in binary
+ * form must reproduce the above copyright notice, this list of conditions and
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. Neither the name of VSB - Technical University of
+ * Ostrava and Graz University of Technology nor the names of its contributors
+ * may be used to endorse or promote products  derived from this software
+ * without specific prior written permission.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL VSB - TECHNICAL UNIVERSITY OF OSTRAVA AND
+ * GRAZ UNIVERSITY OF TECHNOLOGY BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "besthea/besthea.h"
+#include "besthea/uniform_spacetime_be_onthefly_matrix.h"
+
+#include <cstdlib>
+#include <filesystem>
+#include <iostream>
+
+using namespace besthea::mesh;
+using namespace besthea::linear_algebra;
+using namespace besthea::bem;
+using namespace besthea::tools;
+
+struct cauchy_data {
+  static sc dirichlet( sc x1, sc x2, sc x3, const coordinates< 3 > & n, sc t ) {
+    sc norm2 = ( x1 - _y[ 0 ] ) * ( x1 - _y[ 0 ] )
+      + ( x2 - _y[ 1 ] ) * ( x2 - _y[ 1 ] )
+      + ( x3 - _y[ 2 ] ) * ( x3 - _y[ 2 ] );
+    sc value = std::pow( 4.0 * M_PI * _alpha * ( t + _shift ), -1.5 )
+      * std::exp( -norm2 / ( 4.0 * _alpha * ( t + _shift ) ) );
+
+    return value;
+  }
+
+  static sc neumann( sc x1, sc x2, sc x3, const coordinates< 3 > & n, sc t ) {
+    sc dot = ( x1 - _y[ 0 ] ) * n[ 0 ] + ( x2 - _y[ 1 ] ) * n[ 1 ]
+      + ( x3 - _y[ 2 ] ) * n[ 2 ];
+    sc value = ( -1.0 / ( 2.0 * ( t + _shift ) ) ) * dot
+      * dirichlet( x1, x2, x3, n, t );
+
+    return value;
+  }
+
+  static sc initial( sc x1, sc x2, sc x3 ) {
+    sc norm2 = ( x1 - _y[ 0 ] ) * ( x1 - _y[ 0 ] )
+      + ( x2 - _y[ 1 ] ) * ( x2 - _y[ 1 ] )
+      + ( x3 - _y[ 2 ] ) * ( x3 - _y[ 2 ] );
+    sc value = std::pow( 4.0 * M_PI * _alpha * _shift, -1.5 )
+      * std::exp( -norm2 / ( 4.0 * _alpha * _shift ) );
+
+    return value;
+  }
+
+  static constexpr sc _alpha{ 1.0 };
+  static constexpr std::array< sc, 3 > _y{ 1.5, 1.5, 1.5 };
+  static constexpr sc _shift{ 0.0 };
+};
+
+int main( int argc, char * argv[] ) {
+  std::string file;
+
+  // default values
+  file = "../examples/mesh_files/cube_12.txt";
+  int refine = 2;
+  lo n_timesteps = 8;
+  sc end_time = 1.0;
+
+  // read from commandl line
+  if ( argc > 1 ) {
+    file.assign( argv[ 1 ] );
+  }
+  if ( argc > 2 ) {
+    n_timesteps = std::atoi( argv[ 2 ] );
+  }
+  if ( argc > 3 ) {
+    end_time = std::atof( argv[ 3 ] );
+  }
+  if ( argc > 4 ) {
+    refine = std::atoi( argv[ 4 ] );
+  }
+
+  // load spatial mesh from file and refine it
+  triangular_surface_mesh space_mesh;
+  space_mesh.load( file );
+  space_mesh.refine( refine );
+
+  // refine number of timesteps
+  n_timesteps *= std::exp2( 2 * refine );
+
+  // create spacetime mesh as a tensor product of spatial and temporal meshes
+  uniform_spacetime_tensor_mesh spacetime_mesh(
+    space_mesh, end_time, n_timesteps );
+
+  space_mesh.print_info( );
+  spacetime_mesh.print_info( );
+
+  timer t;
+
+  uniform_spacetime_be_space< basis_tri_p0 > space_p0( spacetime_mesh );
+
+  // numerical quadrature orders
+  lo order_sing
+    = 4;  // for singular integrals (adjacent or identical spatial elements)
+  lo order_reg = 4;  // disjoint spatial elements
+
+  // create matrix assembler
+  block_lower_triangular_toeplitz_matrix * V
+    = new block_lower_triangular_toeplitz_matrix( );
+  spacetime_heat_sl_kernel_antiderivative kernel_v( cauchy_data::_alpha );
+  uniform_spacetime_be_assembler assembler_v(
+    kernel_v, space_p0, space_p0, order_sing, order_reg );
+
+  // assemble the matrix
+  t.reset( "V" );
+  assembler_v.assemble( *V );
+  t.measure( );
+
+  besthea::uniform_spacetime_be_onthefly_matrix onthefly(kernel_v, space_p0, space_p0, order_sing, order_reg);
+
+  if(onthefly.check_equal(*V, 1e-6))
+    std::cout << "They are the same\n";
+  else
+    std::cout << "They are different\n";
+
+  delete V;
+  //delete onthefly;
+}
---
+++ include/besthea/uniform_spacetime_be_onthefly_matrix.h
@@ -0,0 +1,177 @@
+
+/** @file block_lower_triangular_toeplitz_matrix.h
+ * @brief
+ */
+
+#ifndef INCLUDE_BESTHEA_UNIFORM_SPACETIME_BE_ONTHEFLY_MATRIX_H_
+#define INCLUDE_BESTHEA_UNIFORM_SPACETIME_BE_ONTHEFLY_MATRIX_H_
+
+#include "besthea/block_matrix.h"
+#include "besthea/full_matrix.h"
+#include "besthea/block_lower_triangular_toeplitz_matrix.h"
+#include "besthea/uniform_spacetime_be_space.h"
+
+#include <array>
+
+
+namespace besthea {
+  template< class kernel_type, class test_space_type, class trial_space_type >
+  class uniform_spacetime_be_onthefly_matrix;
+}
+
+
+
+template< class kernel_type, class test_space_type, class trial_space_type >
+class besthea::uniform_spacetime_be_onthefly_matrix
+  : public besthea::linear_algebra::block_matrix
+{
+private:
+  struct quadrature_wrapper {
+    std::array< std::vector< sc, besthea::allocator_type< sc > >, 4 >
+      _x1_ref;  //!< First coordinates of quadrature nodes in (0,1)x(0,1-x1) to
+                //!< be mapped to the test element
+    std::array< std::vector< sc, besthea::allocator_type< sc > >, 4 >
+      _x2_ref;  //!< Second coordinates of quadrature nodes in (0,1)x(0,1-x1) to
+                //!< be mapped to the test element
+
+    std::array< std::vector< sc, besthea::allocator_type< sc > >, 4 >
+      _y1_ref;  //!< First coordinates of quadrature nodes in (0,1)x(0,1-x1) to
+                //!< be mapped to the trial element
+    std::array< std::vector< sc, besthea::allocator_type< sc > >, 4 >
+      _y2_ref;  //!< Second coordinates of quadrature nodes in (0,1)x(0,1-x1) to
+                //!< be mapped to the trial element
+
+    std::array< std::vector< sc, besthea::allocator_type< sc > >, 4 >
+      _w;  //!< Quadrature weights including transformation Jacobians
+
+    std::vector< sc, besthea::allocator_type< sc > >
+      _x1;  //!< First coordinates of quadrature nodes in the test element
+    std::vector< sc, besthea::allocator_type< sc > >
+      _x2;  //!< Second coordinates of quadrature nodes in the test element
+    std::vector< sc, besthea::allocator_type< sc > >
+      _x3;  //!< Third coordinates of quadrature nodes in the test element
+
+    std::vector< sc, besthea::allocator_type< sc > >
+      _y1;  //!< First coordinates of quadrature nodes in the trial element
+    std::vector< sc, besthea::allocator_type< sc > >
+      _y2;  //!< Second coordinates of quadrature nodes in the trial element
+    std::vector< sc, besthea::allocator_type< sc > >
+      _y3;  //!< Third coordinates of quadrature nodes in the trial element
+
+    std::vector< sc, besthea::allocator_type< sc > >
+      _kernel_values;  //!< Buffer for storing kernel values.
+    std::vector< sc, besthea::allocator_type< sc > >
+      _kernel_values_2;  //!< Buffer for storing additional kernel values.
+  };
+
+public:
+  using matrix_type = besthea::linear_algebra::full_matrix;  //!< Matrix type.
+  using block_vector_type
+    = besthea::linear_algebra::block_vector;            //!< Block vector type.
+  using vector_type = besthea::linear_algebra::vector;  //!< Vector type.
+
+
+
+  uniform_spacetime_be_onthefly_matrix( kernel_type & kernel,
+    test_space_type & test_space, trial_space_type & trial_space,
+    int order_singular = 4, int order_regular = 4 );
+
+  uniform_spacetime_be_onthefly_matrix(
+    const uniform_spacetime_be_onthefly_matrix & that )
+    = delete;
+  
+  virtual ~uniform_spacetime_be_onthefly_matrix( );
+
+
+
+  sc get( lo d, lo i, lo j ) ;
+
+  virtual void apply( const block_vector_type & x, block_vector_type & y,
+   bool trans = false, sc alpha = 1.0, sc beta = 0.0 ) const override;
+
+  void print( std::ostream & stream = std::cout ) const;
+
+  void print_info( ) const {
+    std::cout
+      << "besthea::linear_algebra::uniform_spacetime_be_onthefly_matrix"
+      << std::endl;
+    std::cout << "  number of blocks: " << _block_dim << std::endl;
+    std::cout << "  dimension of each block: " << _dim_domain
+              << " x " << _dim_range << std::endl;
+  }
+
+  bool check_equal(besthea::linear_algebra::block_lower_triangular_toeplitz_matrix & assembled, sc epsilon) ;
+
+private:
+
+  void init_quadrature();
+
+  void get_type( lo i_test, lo i_trial, int & type_int, int & rot_test,
+    int & rot_trial ) const;
+
+  void triangles_to_geometry( const linear_algebra::coordinates< 3 > & x1,
+    const linear_algebra::coordinates< 3 > & x2,
+    const linear_algebra::coordinates< 3 > & x3,
+    const linear_algebra::coordinates< 3 > & y1,
+    const linear_algebra::coordinates< 3 > & y2,
+    const linear_algebra::coordinates< 3 > & y3, int type_int, int rot_test,
+    int rot_trial) ;
+
+  sc get_value(lo delta, lo i_test, lo i_trial, bool special = false) ;
+  
+  void hypercube_to_triangles( sc ksi, sc eta1, sc eta2, sc eta3,
+    int n_shared_vertices, int simplex, sc & x1_ref, sc & x2_ref, sc & y1_ref,
+    sc & y2_ref, sc & jacobian ) const {
+    switch ( n_shared_vertices ) {
+      case 1:
+        hypercube_to_triangles_vertex( ksi, eta1, eta2, eta3, simplex, x1_ref,
+          x2_ref, y1_ref, y2_ref, jacobian );
+        break;
+      case 2:
+        hypercube_to_triangles_edge( ksi, eta1, eta2, eta3, simplex, x1_ref,
+          x2_ref, y1_ref, y2_ref, jacobian );
+        break;
+      case 3:
+        hypercube_to_triangles_identical( ksi, eta1, eta2, eta3, simplex,
+          x1_ref, x2_ref, y1_ref, y2_ref, jacobian );
+        break;
+      case 0:
+      default:
+        return;
+    }
+  }
+  
+  void hypercube_to_triangles_vertex( sc ksi, sc eta1, sc eta2, sc eta3,
+    int simplex, sc & x1_ref, sc & x2_ref, sc & y1_ref, sc & y2_ref,
+    sc & jacobian ) const;
+    
+  void hypercube_to_triangles_edge( sc ksi, sc eta1, sc eta2, sc eta3,
+    int simplex, sc & x1_ref, sc & x2_ref, sc & y1_ref, sc & y2_ref,
+    sc & jacobian ) const;
+
+  void hypercube_to_triangles_identical( sc ksi, sc eta1, sc eta2, sc eta3,
+    int simplex, sc & x1_ref, sc & x2_ref, sc & y1_ref, sc & y2_ref,
+    sc & jacobian ) const;
+
+private:
+
+  quadrature_wrapper my_quadrature;
+  kernel_type * _kernel;
+  test_space_type * _test_space;
+  trial_space_type * _trial_space;
+  int _order_singular;
+  int _order_regular;
+  
+
+  static constexpr std::array< int, 5 > map{ 0, 1, 2, 0,
+    1 };  //!< Auxiliary array for mapping DOFs under
+          // rotation (regularized quadrature). Performs fast modulo 3.
+
+  static constexpr std::array< int, 4 > n_simplices{ 1, 2, 5,
+    6 };  //!< Number of simplices for all configurations (disjoint, shared
+          // vertex, shared edge, identical).
+
+};
+
+
+#endif /* INCLUDE_BESTHEA_UNIFORM_SPACETIME_BE_ONTHEFLY_MATRIX_H_ */
