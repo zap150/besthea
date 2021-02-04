cc198cf5ca7680bfb4d8a36101f2d5064b2acb9d
Jakub Homola <jakub.homola@vsb.cz>
2021-02-04 23:46:55 +0100

    fixed partial override warning

--- include/besthea/uniform_spacetime_be_onthefly_matrix_cpu.h
+++ include/besthea/uniform_spacetime_be_onthefly_matrix_cpu.h
@@ -87,6 +87,8 @@ public:
   using block_vector_type
     = besthea::linear_algebra::block_vector;            //!< Block vector type.
   using vector_type = besthea::linear_algebra::vector;  //!< Vector type.
+  using distributed_block_vector_type
+    = besthea::linear_algebra::distributed_block_vector;  //!< Block vector type.
 
 
 
@@ -111,6 +113,11 @@ public:
 
   virtual void apply( const block_vector_type & x, block_vector_type & y,
    bool trans = false, sc alpha = 1.0, sc beta = 0.0 ) const override;
+
+  virtual void apply( [[maybe_unused]] const distributed_block_vector_type & x,
+    [[maybe_unused]] distributed_block_vector_type & y,
+    [[maybe_unused]] bool trans = false, [[maybe_unused]] sc alpha = 1.0,
+    [[maybe_unused]] sc beta = 0.0 ) const override {};
 
   void apply_cpu( const block_vector_type & x, block_vector_type & y,
    bool trans = false, sc alpha = 1.0, sc beta = 0.0 ) const;
