90a83d985a9254829b3ca9436e9fcd15c5d98649
Jakub Homola <jakub329homola@gmail.com>
2021-04-28 05:40:51 +0200

    load balancing asus

---
+++ experiments/_scripts/loadBalancing/loadbalancing_asus.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# run from the build directory
+
+quadr_order=4
+threads=8
+ver=2
+
+export KMP_AFFINITY=granularity=core,compact
+export OMP_NUM_THREADS=${threads}
+
+
+for reflvl in 7
+do
+
+    ./experiments/matrix_vector_product ${reflvl} ${ver} 20 0 ${quadr_order} ${quadr_order} > ../experiments/_results/loadBalancing/loadbalancing_asus_qo${quadr_order}/loadbalancing_reflvl${reflvl}.txt
+
+done
+
+
