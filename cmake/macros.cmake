147cea97ad09332d07b323180deb62d82f50c292
Jakub Homola <jakub.homola@vsb.cz>
2021-10-01 17:36:36 +0200

    using add_compile_options again, mostly reverted the previous changes, not setting cudastandard

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -7,14 +7,6 @@ macro(check_insource)
 endmacro()
 
 macro(setup_compiler)
-  # can't use add_compile_options, because cmake uses them for all langueges
-  # and nvcc does not understand these intel-specific options
-  #
-  # using MY_ADDITIONAL_CXX_FLAGS helper variable to make passing 
-  # the same options to CXX compiler and CUDA host compiler simpler
-
-  set(MY_ADDITIONAL_CXX_FLAGS "")
-
   if (CMAKE_CXX_COMPILER_ID MATCHES GNU)
     message(STATUS "Using GNU ${CMAKE_CXX_COMPILER_VERSION} toolchain")
 
@@ -23,13 +15,14 @@ macro(setup_compiler)
         " compiled only with g++ 8.3.0 or higher")
     endif()
 
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -Wall -Wextra")
-    # can't pass this also to the cuda host compiler via -Xcompiler
-    # because of unsolvable errors
+    add_compile_options(-Wall -Wextra)
+    # can't use add_compile_options here, need to pass this only to c++ compiler
+    # gcc, as nvcc's host compiler, produces some of such errors, which originate
+    # from some nvcc preprocessing
     string(APPEND CMAKE_CXX_FLAGS " -pedantic-errors")
 
     # GNU cannot vectorise complicated loops
-    #string(APPEND MY_ADDITIONAL_CXX_FLAGS " -fopt-info-omp-vec-optimized-missed")
+    #add_compile_options(-fopt-info-omp-vec-optimized-missed)
 
   elseif (CMAKE_CXX_COMPILER_ID MATCHES AppleClang)
     message(STATUS "Using AppleClang ${CMAKE_CXX_COMPILER_VERSION} toolchain")
@@ -39,9 +32,9 @@ macro(setup_compiler)
         " compiled only with AppleClang 12 or higher")
     endif()
 
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -Wall -Wextra -pedantic-errors")
+    add_compile_options(-Wall -Wextra -pedantic-errors)
     # Clang cannot vectorise complicated loops
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -Wno-pass-failed")
+    add_compile_options(-Wno-pass-failed)
 
   elseif (CMAKE_CXX_COMPILER_ID MATCHES Clang
     AND NOT CMAKE_CXX_COMPILER_ID MATCHES AppleClang)
@@ -52,17 +45,17 @@ macro(setup_compiler)
         " compiled only with clang++ 7 or higher")
     endif()
 
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -Wall -Wextra -pedantic-errors")
+    add_compile_options(-Wall -Wextra -pedantic-errors)
     # Clang cannot vectorise complicated loops
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -Wno-pass-failed")
-    #string(APPEND MY_ADDITIONAL_CXX_FLAGS
-    #  " -Rpass=\"vect\" -Rpass-missed=\"vect\" -Rpass-analysis=\"vect\"")
+    add_compile_options(-Wno-pass-failed)
+    #add_compile_options(
+    #  -Rpass="vect" -Rpass-missed="vect" -Rpass-analysis="vect")
     if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 11)
       # TODO: get rid of this warning (don't understand it)
-      string(APPEND MY_ADDITIONAL_CXX_FLAGS " -Wno-dtor-name")
+      add_compile_options(-Wno-dtor-name)
     endif()
 
-    string(APPEND CMAKE_CUDA_FLAGS " -Wno-overlength-strings")
+    add_compile_options(-Wno-overlength-strings)
 
   elseif (CMAKE_CXX_COMPILER_ID MATCHES Intel)
     message(STATUS "Using Intel ${CMAKE_CXX_COMPILER_VERSION} toolchain")
@@ -72,30 +65,32 @@ macro(setup_compiler)
         " compiled only with icpc 19.0.1 or higher")
     endif()
 
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -w3")
-    #string(APPEND MY_ADDITIONAL_CXX_FLAGS " -qopt-report=5 -qopt-report-phase=vec")
+    add_compile_options(-w3)
+    #add_compile_options(-qopt-report=5 -qopt-report-phase=vec)
     # zero used for undefined preprocessing identifier
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -diag-disable 193")
+    add_compile_options(-diag-disable=193)
     # attribute appears more than once
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -diag-disable 2620")
+    add_compile_options(-diag-disable=2620)
     # parameter was never referenced
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -diag-disable 869")
+    add_compile_options(-diag-disable=869)
     # declaration hides variable
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -diag-disable 1599")
+    add_compile_options(-diag-disable=1599)
     # value copied to temporary, reference to temporary used
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -diag-disable 383")
+    add_compile_options(-diag-disable=383)
     # inlining inhibited by limit max-total-size
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -diag-disable 11074")
+    add_compile_options(-diag-disable=11074)
     # to get full report use -qopt-report=4 -qopt-report-phase ipo
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -diag-disable 11076")
+    add_compile_options(-diag-disable=11076)
     # specified as both a system and non-system include directory
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -diag-disable 2547")
+    add_compile_options(-diag-disable=2547)
     # unrecognised GCC pragma
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -diag-disable 2282")
+    add_compile_options(-diag-disable=2282)
     # floating-point equality and inequality comparisons
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -diag-disable 1572")
+    add_compile_options(-diag-disable=1572)
     # external function definition with no prior declaration
-    string(APPEND MY_ADDITIONAL_CXX_FLAGS " -diag-disable 1418")
+    add_compile_options(-diag-disable=1418)
+    # selector expression is constant
+    add_compile_options(-diag-disable=280)
 
   else()
     message(FATAL_ERROR "Unknown C++ compiler: ${CMAKE_CXX_COMPILER_ID}")
@@ -107,11 +102,6 @@ macro(setup_compiler)
   if (NOT CMAKE_BUILD_TYPE)
     #set(CMAKE_BUILD_TYPE RelWithDebInfo)
     set(CMAKE_BUILD_TYPE Release)
-  endif()
-
-  string(APPEND CMAKE_CXX_FLAGS ${MY_ADDITIONAL_CXX_FLAGS})
-  if(USE_CUDA)
-    string(APPEND CMAKE_CUDA_FLAGS " -Xcompiler \"" ${MY_ADDITIONAL_CXX_FLAGS} "\"")
   endif()
 endmacro()
 
@@ -145,21 +135,15 @@ endmacro()
 
 macro(enable_OpenMP)
   if (CMAKE_CXX_COMPILER_ID MATCHES GNU)
-    set(MY_OMP_FLAG "-fopenmp")
-  elseif (CMAKE_CXX_COMPILER_ID MATCHES Intel)
-    set(MY_OMP_FLAG "-qopenmp")
+    add_compile_options(-fopenmp)
   elseif (CMAKE_CXX_COMPILER_ID MATCHES AppleClang)
-    #set(MY_OMP_FLAG "-I/opt/local/include/libomp -Xclang -fopenmp")
-    set(MY_OMP_FLAG "-Xclang -fopenmp")
+    #add_compile_options(-I/opt/local/include/libomp -Xclang -fopenmp)
+    add_compile_options(-Xclang -fopenmp)
   elseif (CMAKE_CXX_COMPILER_ID MATCHES Clang
-      AND NOT CMAKE_CXX_COMPILER_ID MATCHES AppleClang)
-    set(MY_OMP_FLAG "-fopenmp")
-  endif()
-    
-  string(APPEND CMAKE_CXX_FLAGS " " ${MY_OMP_FLAG})
-
-  if(USE_CUDA)
-    string(APPEND CMAKE_CUDA_FLAGS " -Xcompiler \"" ${MY_OMP_FLAG} "\"")
+    AND NOT CMAKE_CXX_COMPILER_ID MATCHES AppleClang)
+    add_compile_options(-fopenmp)
+  elseif (CMAKE_CXX_COMPILER_ID MATCHES Intel)
+    add_compile_options(-qopenmp)
   endif()
 endmacro()
 
@@ -199,13 +183,6 @@ macro(setup_CUDA)
   endif()
 
   find_package(CUDA REQUIRED)
-
-  if(${CMAKE_VERSION} VERSION_LESS "3.18.0")
-    string(APPEND CMAKE_CUDA_FLAGS " -std=c++17")
-  else()
-    set(CMAKE_CUDA_STANDARD 17)
-    set(CMAKE_CUDA_STANDARD_REQUIRED True)
-  endif()
 
   if(${CMAKE_VERSION} VERSION_LESS "3.18.0")
     string(APPEND CMAKE_CUDA_FLAGS " -arch=compute_60")
